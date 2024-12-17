f7f1a8963e9d6bfb3ef0babcbe1a828c8ed1c233
derrick phang <derrickphb@gmail.com>
2024-12-17 18:40:00 +0800

    17 Dec 2024: -establish netbeans repository.

---
+++ src/main/java/com/wif3006/restaurant/component/order/OrderRepository.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
+ */
+package com.wif3006.restaurant.component.order;
+
+/**
+ *
+ * @author derri
+ */
+public interface OrderRepository {
+    
+}
